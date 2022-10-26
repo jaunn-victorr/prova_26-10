8d48b6df82d753bc64b20da031b4bea197d673f7
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-10-26 17:27:22 +0000

    Derrota.jsp

---
+++ app/src/main/java/joaogrunenberg/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package joaogrunenberg.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import joaogrunenberg.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
+    /* @Copy Github 20221025 */
+}
